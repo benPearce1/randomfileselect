afe1ec87f2b75d02481be432334aab3306dda7c6
Ben Pearce <benpearce@yahoo.com>
2014-12-02 07:47:24 +1000

    Added command line options Extension filter is case-insensitive Refactorings Replaced copy option with method option Added list method to only print files and path Added verbose option

--- randomfileselect.py
+++ randomfileselect.py
@@ -1,3 +1,11 @@
+import os
+import shutil
+import getopt
+import sys
+import datetime
+from random import choice, shuffle, randint
+
+# data classes
 class RandomDirectorySource:
 	def __init__(self):
 		self.SourcePath = ''
@@ -6,68 +14,129 @@ class RandomDirectorySource:
 
 class Configuration:
 	def __init__(self):
-		self.hideoriginalnames = True
+		self.hideoriginalnames = False
 		self.filter = list()
-		self.copyfiles = True
+		self.method = 'copy'
+		self.verbose = False
 
-import os
-import shutil
-import getopt
-import sys
-import datetime
-from random import choice, shuffle, randint
+# methods
+def processConfigFile(configfile):
+	with open(configfile) as f:
+		config = f.readlines()
+	for c in config:
+		c = c.strip()
+		if c.startswith('source'):
+			parts = c[c.index('=') + 1:].split(',')
+			randomDir = RandomDirectorySource()
+			randomDir.SourcePath = parts[0]
+			randomDir.DestinationPath = parts[1]
+			randomDir.SelectionCount = int(parts[2])
+			sources.append(randomDir)
+		if c.startswith('hideoriginalnames'):
+			value = c[c.index('=') + 1:].strip()
+			configuration.hideoriginalnames = value.upper() == 'TRUE'
+			print('hideoriginalnames = {0}'.format(configuration.hideoriginalnames))
+		if c.startswith('filter'):
+			value = c[c.index('=') + 1:]
+			configuration.filter = value.split('|')
+		if c.startswith('method'):
+			value = c[c.index('=') + 1:].strip()
+			configuration.method = value.lower()
+			print('method = {0}'.format(configuration.method))
+		if c.startwith('verbose'):
+			value = c[c.index('=') + 1:].strip()
+			configuration.verbose = value.upper() == 'TRUE'
+
+def printHelp():
+	print ('python {0} [-i filename | -f filter -m method -s path -o path -c count -h -v]'.format(os.path.basename(__file__)))
+	print ('\t-i file	: input file name')
+	print ('\t-f filter	: file extension filter, pipe(|) delimited list')
+	print ('\t-m method : operation method (copy, zero, list)')
+	print ('\t-h 		: hide original filenames, output will be sequentially numbered files')
+	print ('\t-s path 	: source path')
+	print ('\t-o path 	: output path')
+	print ('\t-c count 	: number of files to pick')
+	print ('\t-v 		: verbose output')
+
+def printConfig():
+	if configuration.verbose:
+		print ('Running config:')
+		print ('\tMethod: {0}'.format(configuration.method))
+		print ('\tFilter: {0}'.format(configuration.filter))
+		print ('\tHide original filenames: {0}'.format(configuration.hideoriginalnames))
+		print ('Sources:')
+		for s in sources:
+			print ('\tSource path: {0}'.format(s.SourcePath))
+			print ('\tDestionation path: {0}'.format(s.DestinationPath))
+			print ('\tSelection count: {0}'.format(s.SelectionCount))
+
+def validateConfig():
+	if configuration.method in ('zero','touch', 'copy'):
+		for s in sources:
+			if s.DestinationPath == '':
+				print ('Destination path missing')
+				exit(1)
+
+# -- main --
 
 # read configuration file
-configfile = 'randomshow.cfg'
-opts, args = getopt.getopt(sys.argv[1:], 'i:', ['input='])
+opts, args = getopt.getopt(sys.argv[1:], 'i:f:m:ho:s:c:v')
 
-for opt, arg in opts:
-	if opt in ('-i','--input'):
-		configfile = arg
-
-print ('{1} Using configuration file: {0}'.format(configfile,datetime.datetime.now()))		
-with open(configfile) as f:
-	config = f.readlines()
+if len(opts) == 0:
+	printHelp()
 
 sources = list()
 destinationCounter = dict()
 files = list()
 configuration = Configuration()
 
-for c in config:
-	c = c.strip()
-	if c.startswith('source'):
-		parts = c[c.index('=') + 1:].split(',')
-		randomDir = RandomDirectorySource()
-		randomDir.SourcePath = parts[0]
-		randomDir.DestinationPath = parts[1]
-		randomDir.SelectionCount = int(parts[2])
-		sources.append(randomDir)
-	if c.startswith('hideoriginalnames'):
-		value = c[c.index('=') + 1:].strip()
-		configuration.hideoriginalnames = value.upper() == 'TRUE'
-		print('hideoriginalnames = {0}'.format(configuration.hideoriginalnames))
-	if c.startswith('filter'):
-		value = c[c.index('=') + 1:]
-		configuration.filter = value.split('|')
-	if c.startswith('copyfiles'):
-		value = c[c.index('=') + 1:].strip()
-		configuration.copyfiles = value.upper() == 'TRUE'
-		print('copyfiles = {0}'.format(configuration.copyfiles))
-	
+for opt, arg in opts:
+	if opt in ('-i'):
+		configfile = arg
+		print ('{1} Using configuration file: {0}'.format(configfile,datetime.datetime.now()))
+		processConfigFile(configfile)
+	if opt in ('-f'):
+		configuration.filter = arg.split('|')
+	if opt in ('-h'):
+		if len(arg) > 0:
+			configuration.hideoriginalnames = arg.upper() == 'TRUE'
+		else:
+			configuration.hideoriginalnames = True
+	if opt in ('-m'):
+		configuration.method = arg.lower()
+	if opt in ('-s'):
+		if len(sources) == 0:
+			sources.append(RandomDirectorySource())
+		print (arg)
+		sources[0].SourcePath = arg
+	if opt in ('-o'):
+		if len(sources) == 0:
+			sources.append(RandomDirectorySource())
+		sources[0].DestinationPath = arg
+	if opt in ('-c'):
+		if len(sources) == 0:
+			sources.append(RandomDirectorySource())
+		sources[0].SelectionCount = int(arg)
+	if opt in ('-v'):
+		configuration.verbose = True
+
+printConfig()
+validateConfig()
+
 for s in sources:
 	files = list()
-	if s.DestinationPath in destinationCounter:
-		destinationCounter[s.DestinationPath] += 1
-	else:
-		destinationCounter[s.DestinationPath] = 1
-		for f in os.listdir(s.DestinationPath):
-			os.remove(os.path.join(s.DestinationPath,f))
-		
+	if s.DestinationPath != '':
+		if s.DestinationPath in destinationCounter:
+			destinationCounter[s.DestinationPath] += 1
+		else:
+			destinationCounter[s.DestinationPath] = 1
+			for f in os.listdir(s.DestinationPath):
+				os.remove(os.path.join(s.DestinationPath,f))
+
 	for dirname, dirnames, filenames in os.walk(s.SourcePath):
 		for filename in filenames:
 			for type in configuration.filter:
-				if filename.endswith(type):
+				if filename.upper().endswith('.{0}'.format(type.upper())):
 					files.append(os.path.join(dirname, filename))
 
 		# Advanced usage:
@@ -75,20 +144,24 @@ for s in sources:
 		if '.git' in dirnames:
 			# don't go into any .git directories.
 			dirnames.remove('.git')
-	
+
 	shuffle(files)
 
 	# make it more random, shuffle it again
 	shuffle(files)
-	
+
 	# create destination files
+	# for f in files:
+	# 	print (f)
 	for i in range(0,s.SelectionCount):
 		if len(files) > 0:
 			file = files.pop()
-			if configuration.copyfiles:
+			if configuration.method in ('copy'):
 				print ("{0} Copying {1} to {2}".format(datetime.datetime.now(), file, s.DestinationPath))
 				shutil.copy2(file, s.DestinationPath)
-			else:
+			elif configuration.method in ('list'):
+				print (file)
+			elif configuration.method in ('zero','touch'):
 				print ("{0} Touching {1} in {2}".format(datetime.datetime.now(),filename,s.DestinationPath))
 				open(os.path.join(s.DestinationPath,file),'w').close()
 
@@ -106,4 +179,5 @@ if configuration.hideoriginalnames:
 				os.rename(os.path.join(d,file), newFile)
 				counter += 1
 
-print("{0} Done!".format(datetime.datetime.now()))
+if configuration.verbose:
+	print("{0} Done!".format(datetime.datetime.now()))
